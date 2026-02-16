d78bf9a6c5d1091cf582c1817162cf3845e8d489
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2026-02-16 20:50:15 -0300

    feat: adição do backend

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Seja bem-vindo à burgueria!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor conectado na porta ${PORT}`);
+});
