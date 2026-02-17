5cf5d24d8111b9d11eb5f88a2c01fd09e0bb0634
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2026-02-17 12:46:29 -0300

    feat: melhorias no server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
+    res.send('Seja bem-vindo à burgueria!');
+});
+
+app.post('/', (req, res) => {
+    res.send('Seja bem-vindo à burgueria!');
+});
+
+app.put('/', (req, res) => {
+    res.send('Seja bem-vindo à burgueria!');
+});
+
+app.delete('/', (req, res) => {
     res.send('Seja bem-vindo à burgueria!');
 });
 
