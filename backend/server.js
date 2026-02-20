b6d4d825a92502d92638a152735decc781a428cb
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2026-02-19 23:40:28 -0300

    feat: conectando o front ao backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors'
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/usuarios', useRoutes);
 
--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-const toggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('nav-menu');
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-menu");
 
-toggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
 });
+
+document
+  .getElementById("login-form")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault(); // evita recarregar a página
+
+    const dados = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
+      adress: document.getElementById("adress").value,
+    };
+
+    const resposta = await fetch("http://localhost:3000/usuarios/cadastro", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dados),
+    });
+
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("password").value = "";
+      document.getElementById("adress").value = "";
+
+    const resultado = await resposta.json();
+    alert(resultado.resposta);
+  });
